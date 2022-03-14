a25bde7261dc6d05062991171f0911158e20efb2
santhoshrajneu <100248227+santhoshrajneu@users.noreply.github.com>
2022-03-13 23:52:25 -0400

    Add files via upload

---
+++ putty.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#install putty using
+sudo apt install putty
+#on the server side, install openssh-server
+#sudo apt-get install openssl-server
+#above thing is to be done on the server side
+
+#open putty
+putty
+
+
+#! /bin/bash
+sudo reboot
+
+
+
